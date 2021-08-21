ba3e2c6915f4ebbe28946b7e2ed9a5470954baea
Arifuzzaman Munaf <arifuzzamanmunaf@gmail.com>
2021-08-21 15:14:23 +0600

    fetched data using JsonPath

--- GET_Request/FetchUserData.py
+++ GET_Request/FetchUserData.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import jsonpath
 
 
 """API Url"""
@@ -14,4 +16,20 @@ print("Contents\n",response.content)
 print("____________________________________________")
 
 """Displaying the header of Response"""
-print("Header\n",response.headers)
+print("Header\n", response.headers)
+
+print("____________________________________________")
+"""Parse response to Json format"""
+json_response = json.loads(response.text)
+
+print("Json Format\n", json_response)
+
+print("____________________________________________")
+
+"""Fetch values using JsonPath"""
+pages = jsonpath.jsonpath(json_response, 'total_pages')
+print("total pages =", *pages)
+
+
+"""testing whether it shows error or not"""
+assert pages[0] == 2
